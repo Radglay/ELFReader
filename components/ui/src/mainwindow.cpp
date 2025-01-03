5aee7c13b69df37b2425d76c074f73ce0fec265a
Radglay <lukaszdyrlaga02@gmail.com>
2025-01-03 14:08:29 +0100

    Add ElfPart

--- components/ui/src/mainwindow.cpp
+++ components/ui/src/mainwindow.cpp
@@ -6,7 +6,6 @@
 #include <fstream>
 #include <filesystem>
 #include "NavigationPanel.h"
-#include "ReadingFileContentUtility.hpp"
 #include <ElfPart.hpp>
 #include <QVBoxLayout>
 #include "StructureDetails.h"
@@ -23,45 +22,12 @@ MainWindow::MainWindow(QWidget *parent)
 
     auto l_topLayout = new QHBoxLayout();
 
-
-    // navbar -> sections, symbols etc...
     setAttribute(Qt::WA_TransparentForMouseEvents);
     m_navigationPanel = new NavigationPanel();
 
     m_objectCodeContent = new ObjectCodeContent();
 
     m_structureDetails = new StructureDetails();
-
-    // FileHeader -> 0, sizeof(FileHeader)
-    // SectionHeaders -> (offset = FileHeader.shoff; for-each: offset += sizeof(T), T)
-    // ProgramHeaders -> (offset = FileHeader.phoff; for-each: offset += sizeof(T), T)
-
-    // IElfSections -> (SectionHeader* member, offset = member.sh_offset; [IF EXIST] for-each children: offset += sizeof(T), T)
-    // with children: rel, rela, verneed, sym, strTab??? -> from child map[offset, str] + member.sh_offset
-
-
-    // Member SectionHeader pointer, store or not? (DTO)
-    // WHAT about jumping to / storing info about LINKED sections? member.sh_info, member.sh_link???
-
-    //    l_fileHeaderPart.type = ElfPartType::FileHeader;
-//    l_fileHeaderPart.
-
-    auto l_fileSize { std::filesystem::file_size("../main") };
-    qDebug() << l_fileSize;
-
-    std::ifstream l_file;
-    l_file.open("../main", std::ios_base::out | std::ios_base::binary);
-    m_fileContent = new char[l_fileSize];
-    l_file.read(m_fileContent, l_fileSize);
-    m_fileSize = l_fileSize;
-
-    l_file.seekg(0);
-    auto l_elfParts { readElfPartsFromFile(&l_file) };
-
-    qDebug() << "test1";
-    m_navigationPanel->loadNavigationItems(l_elfParts);
-    qDebug() << "test2";
-    m_objectCodeContent->loadContent(m_fileContent, m_fileSize);
 
     l_topLayout->addWidget(m_navigationPanel);
     l_topLayout->addWidget(m_objectCodeContent);
